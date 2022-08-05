23688438aa7c71cdcce436698c911b511febea0e
Zulpikar <zulpikaruygur@cybertekschool.com>
2022-08-05 21:12:40 +0300

    day 3 third push-- check box button

--- src/test/java/Day3/RadioBtn.java
+++ src/test/java/Day3/RadioBtn.java
@@ -3,6 +3,8 @@ package Day3;
 import org.junit.*;
 import org.openqa.selenium.*;
 
+import java.util.*;
+
 public class RadioBtn extends BasePage {
 
     @Before
@@ -15,5 +17,16 @@ public class RadioBtn extends BasePage {
         WebElement radtn1 = driver.findElement(By.id("red"));
         radtn1.click();
         System.out.println(radtn1.getText());
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+        List<WebElement> btns = driver.findElements(By.xpath("//input[@type=\"radio\"]"));
+        for (WebElement btn: btns) {
+            btn.click();
+            System.out.println(btn.getText());
+            Thread.sleep(1000);
+
+        }
     }
 }
